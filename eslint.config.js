4be0dbac5b1322ecc4190ff2e4dbb5ffd31a21ed
rmLappy <gotthecake@hotmail.co.uk>
2025-10-22 09:42:44 +0700

    style: formatted files to pass Prettier check

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,36 @@
-import js from "@eslint/js";
-import parser from "@typescript-eslint/parser";
-import plugin from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-config-prettier";
+import js from '@eslint/js';
+import parser from '@typescript-eslint/parser';
+import plugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-config-prettier';
 
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     languageOptions: {
       parser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
       globals: {
-        console: "readonly",
-        process: "readonly",
-        module: "readonly",
-        require: "readonly",
-        __dirname: "readonly",
-        exports: "readonly"
-      }
+        console: 'readonly',
+        process: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        exports: 'readonly',
+      },
     },
     plugins: {
-      "@typescript-eslint": plugin
+      '@typescript-eslint': plugin,
     },
     rules: {
       ...plugin.configs.recommended.rules,
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "no-console": "off"
-    }
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'no-console': 'off',
+    },
   },
-  prettier
+  prettier,
 ];
