a8cabd4642126d38884bf1d97ac095840a8ce8cf
rmLappy <gotthecake@hotmail.co.uk>
2025-10-23 10:29:40 +0700

    chore: fixed TS folder structure and config to align with CI

--- api/src/tests/healthcheck.test.ts
+++ api/src/tests/healthcheck.test.ts
@@ -1,17 +1,21 @@
 // basic health check
 
 import assert from 'assert';
-import fetch from 'node-fetch';
 
-// ask gitHUB api for data
-async function runHealthCheck() {
-  const response = await fetch('https://api.github.com');
+type GitHubAPIResponse = {
+  current_user_url: string;
+  [key: string]: unknown; // allow extra keys without strict failure
+};
+
+// ask GitHub API for data
+async function runHealthCheck(): Promise<void> {
+  const response: Response = await fetch('https://api.github.com');
 
   // see if returns exactly 200 / ok
   assert.strictEqual(response.status, 200, 'Did not get 200 response from API');
 
   // convert body of reply to JS object
-  const data = await response.json();
+  const data: GitHubAPIResponse = await response.json();
 
   // check the returned data fits known shape
   assert.ok(data.current_user_url, 'Data not a core Github field');
@@ -21,7 +25,7 @@ async function runHealthCheck() {
 }
 
 // Run the above. If anything threw an error, mark the process as failed (for CI)
-runHealthCheck().catch((err) => {
+runHealthCheck().catch((err: Error) => {
   console.error('❌ Health check failed with error message:', err.message);
-  process.exit(1); // ping CI with none zero code so it fails / flags
+  process.exit(1); // ping CI with non-zero code so it fails / flags
 });
